b50fbd9559d616378e59261aa8a998e6867ff076
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-10 20:57:52 +0700

    Thêm giải thích về thuật toán BFS

---
+++ Uninformed_Search/Breadth_First_Search.py
@@ -0,0 +1,4 @@
+# Tìm kiếm theo chiều rộng là đi qua các đỉnh, gần với đỉnh ban đầu nhất, nếu không phải đỉnh cần tìm
+# -->ghi giá trị của đỉnh vào hàng đợi
+#Tiếp tục dựa trên hàng đợi và truy cập đến các đỉnh tiếp theo và lặp lại chương trình
+#Tùy thuộc vào hình dạng đồ thì sẽ có từng cách tiếp cận khác nhau.
