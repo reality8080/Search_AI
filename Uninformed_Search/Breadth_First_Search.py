8145b34d4e54661d0953acefbeb6239204fa3c85
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-10 22:13:21 +0700

    Add Matrix

--- Uninformed_Search/Breadth_First_Search.py
+++ Uninformed_Search/Breadth_First_Search.py
@@ -2,3 +2,16 @@
 # -->ghi giá trị của đỉnh vào hàng đợi
 #Tiếp tục dựa trên hàng đợi và truy cập đến các đỉnh tiếp theo và lặp lại chương trình
 #Tùy thuộc vào hình dạng đồ thì sẽ có từng cách tiếp cận khác nhau.
+# Giả sử ta dùng ma trận toán học dòng, cột để biểu diễn đồ thị giữa 5 thành phố
+# ---A=[
+#           [0,0,0,0,0],
+#           [0,0,0,0,0],
+#           [0,0,0,0,0],
+#           [0,0,0,0,0],
+# ]
+#   0 1 2 3 4
+# 0 0 0 0 0 0 
+# 1 0 0 0 0 0 
+# 2 0 0 0 0 0 
+# 3 0 0 0 0 0 
+# 4 0 0 0 0 0 
