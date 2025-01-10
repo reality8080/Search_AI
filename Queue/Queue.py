51cf396e62fa7af37403c3541f1da89069f19585
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-10 22:04:44 +0700

    Add Queue and deque

--- Queue/Queue.py
+++ Queue/Queue.py
@@ -1,4 +1,4 @@
-# Khai báo hàng đợi
+# Khai báo hàng đợi như list
 q=[]
 # Chèn giá trị
 q.insert(0,1)
@@ -9,6 +9,18 @@ q.insert(0,5)
 q.insert(0,6)
 q.insert(0,7)
 q.insert(0,8)# cùng đặt ở một vị trí thì đẩy vị trí trước đó ra phía sau và chèn giá trị mới 
+q.append(9)#Đặt 9 ở sau cùng
 print(q)
 print(q.pop())# Lấy giá trị đầu tiên bên trái
+print(q)
+
+# Tạo queue thông qua Deque
+from collections import deque
+q=deque()
+q.appendleft(1)
+q.appendleft(2)
+q.appendleft(3)
+q.appendleft(4)
+print(q)
+print(q.pop())
 print(q)
