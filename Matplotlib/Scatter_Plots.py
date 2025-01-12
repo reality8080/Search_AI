77f13d398892da9f401e2553784ab0ed08ba9515
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-12 21:36:03 +0700

    Add Scatter_Plots

---
+++ Matplotlib/Scatter_Plots.py
@@ -0,0 +1,13 @@
+#Học matplotlib python
+#https://www.youtube.com/watch?v=OZOOLe2imFo
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+X_data=np.random.random(1000)*100
+Y_data=np.random.random(1000)*100
+plt.scatter(X_data,Y_data,color='#f0f', s=100, marker="*", alpha=0.5)
+
+plt.legend()
+plt.grid()
+plt.show()
