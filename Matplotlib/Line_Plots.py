7a19fbf87302d2727dc267f54d8dddda0dc4be8c
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-12 21:47:45 +0700

    Add Line Plots

---
+++ Matplotlib/Line_Plots.py
@@ -0,0 +1,16 @@
+#Học matplotlib python
+#https://www.youtube.com/watch?v=OZOOLe2imFo
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+X_data=[2006+X for X in range(14)]
+Y_data=np.random.random(14)*100
+
+# plt.plot(X_data,Y_data, color='red', lw=5, linestyle="--")
+plt.plot(X_data,Y_data,"r--",lw=4)
+
+plt.legend()
+
+plt.grid()
+plt.show()
