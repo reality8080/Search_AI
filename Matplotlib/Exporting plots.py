2d0f05fb2cb9ddb0543638158226cb910a88ee44
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-14 19:23:58 +0700

    Add Exploting plots

---
+++ Matplotlib/Exporting plots.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+x=np.arange(100)
+
+fig, axs=plt.subplots(2,2)
+
+axs[0,0].plot(x,np.sin(x))
+
+axs[0,1].plot(x,np.cos(x))
+
+axs[1,0].plot(x,np.tan(x))
+
+axs[1,1].plot(x,np.log(x))
+
+plt.grid()
+# plt.show()
+plt.savefig("exportingPlots.png", 
+            dpi=1080,
+            transparent=True)
