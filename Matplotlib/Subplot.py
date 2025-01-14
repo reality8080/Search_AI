a67068d6a7abfc20c247de5d0dfc4f3e24800d3c
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-14 19:07:17 +0700

    Add Subplot

---
+++ Matplotlib/Subplot.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=np.arange(100)
+
+fig, axs=plt.subplots(2,2)
+
+# Chia làm 4 đồ thị(2*2)
+
+axs[0,0].plot(x,np.sin(x))
+
+axs[0,1].plot(x,np.cos(x))
+
+axs[1,0].plot(x,np.tan(x))
+
+axs[1,1].plot(x,np.log(x))
+
+plt.grid()
+plt.show()
