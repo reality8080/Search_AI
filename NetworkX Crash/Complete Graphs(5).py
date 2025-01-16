f917dcd8308c74cd3597e942b4aacf180613a798
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-16 15:20:23 +0700

    Add Complete Graphs

--- NetworkX Crash/Complete Graphs(5).py
+++ NetworkX Crash/Complete Graphs(5).py
@@ -1,9 +1,12 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G=nx.complete_graph(8)
+G=nx.complete_graph(12)
 
-# nx.draw_spring(A,with_labels=True)
-# plt.show()
-nx.draw_planar(G,with_labels=True)
+# nx.draw_spring(G,with_labels=True)
+# nx.draw_planar(G,with_labels=True)
+#draw_planar: complete_graph phải <=4
+# nx.draw_random(G,with_labels=True)
+nx.draw_kamada_kawai(G,with_labels=True)
+# ..., còn rất nhiều cách vẽ.
 plt.show()
