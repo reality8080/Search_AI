ad40a92e04e1de16e0563c58ebf2f4d7a26d1eb0
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-16 16:36:05 +0700

    Add Centrality

---
+++ NetworkX Crash/Centrality(8).py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# edge_list=[(1,2),(1,3),(1,4),(1,5),(1,6),(2,3),(3,4),(4,5),(5,6),(6,2)]
+
+# # G=nx.DiGraph()
+# G=nx.Graph()
+
+
+# G.add_edges_from(edge_list)
+
+
+# print(nx.degree_centrality(G))
+# # Tính toán tầm quan trọng của một đỉnh dựa trên 
+# # số lượng kết nối trực tiếp (bậc) của nó so với 
+# # tổng số đỉnh trong đồ thị trừ đi 1.
+# print(nx.closeness_centrality(G))
+# # Đo lường mức độ "gần gũi" của một đỉnh đến tất 
+# # cả các đỉnh khác trong đồ thị. Đỉnh càng gần 
+# # (ít bước di chuyển hơn) đến các đỉnh khác thì
+# # giá trị càng cao.
+# print(nx.eigenvector_centrality(G))
+# #  Đo lường tầm quan trọng của một đỉnh không chỉ 
+# # dựa vào số kết nối trực tiếp, mà còn dựa vào tầm
+# # quan trọng của các đỉnh mà nó kết nối. Đỉnh được 
+# # kết nối với các đỉnh "quan trọng" sẽ có giá trị 
+# # cao hơn.
+# print(nx.betweenness_centrality(G))
+# #  Đo lường tầm quan trọng của một đỉnh dựa trên số
+# # lượng đường đi ngắn nhất giữa các cặp đỉnh khác đi
+# # qua đỉnh đó. Đỉnh đóng vai trò "cầu nối" sẽ có giá
+# # trị cao hơn
+
+
+# nx.draw_spring(G,with_labels=True)
+# plt.show()
+
+G1=nx.complete_graph(5)
+G2=nx.complete_graph(5)
+G2=nx.relabel_nodes(G2,{0:"A",1:"B",2:"C",3:"D",4:"E",5:"F"})
+G_connector=nx.from_edgelist([(4,"X"),("A","X")])
+G=nx.compose_all([G1,G2,G_connector])
+
+print(nx.degree_centrality(G))
+print(nx.betweenness_centrality(G))
+
+print(nx.density(G))
+print(nx.diameter(G))
+
+nx.draw_spring(G,with_labels=True)
+plt.show()
+
--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 - Matplotlib
-https://matplotlib.org/stable/gallery/index.html
-https://matplotlib.org/stable/api/index.html
-https://matplotlib.org/stable/tutorials/index.html
+    https://matplotlib.org/stable/gallery/index.html
+    https://matplotlib.org/stable/api/index.html
+    https://matplotlib.org/stable/tutorials/index.html
 - Học matplotlib python
-https://www.youtube.com/watch?v=OZOOLe2imFo
+    https://www.youtube.com/watch?v=OZOOLe2imFo 
 - Học BFS
-https://www.youtube.com/watch?v=7XVTnCrWDPY
+    https://www.youtube.com/watch?v=7XVTnCrWDPY
 - NetworkX
-https://networkx.org/documentation/networkx-3.4.2/tutorial.html
+    https://networkx.org/documentation/networkx-3.4.2/tutorial.html
+
+    https://networkx.org/documentation/stable/reference/algorithms/centrality.html
+
+    https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.centrality.closeness_centrality.html#networkx.algorithms.centrality.closeness_centrality
+
+    https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.centrality.eigenvector_centrality.html#networkx.algorithms.centrality.eigenvector_centrality
+
