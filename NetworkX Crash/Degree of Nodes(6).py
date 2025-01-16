45699ba6e714fdbf5181d2fd11cfb0a2721af9ca
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-16 15:32:56 +0700

    Degree of Nodes

---
+++ NetworkX Crash/Degree of Nodes(6).py
@@ -0,0 +1,19 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+edge_list=[(1,2),(1,3),(1,4),(1,5),(1,6),(2,3),(3,4),(4,5),(5,6),(6,2)]
+
+G=nx.DiGraph()
+
+G.add_edges_from(edge_list)
+
+print(dict(G.degree)[1])
+#In số cạnh quanh node, Graph
+print(dict(G.in_degree)[1])
+#In số vector quanh node, đi vào, DiGraph
+print(dict(G.out_degree)[1])
+#In số vector quanh node, đi ra, DiGraph
+
+# Đường đi ngắn nhất
+nx.draw_spring(G,with_labels=True)
+plt.show()
