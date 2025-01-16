e6149ef5cc0e51535435697caf7f786da9440d4d
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-16 17:19:14 +0700

    Add Bridges

--- NetworkX Crash/Bridges(12).py
+++ NetworkX Crash/Bridges(12).py
@@ -4,10 +4,14 @@ import networkx as nx
 G1=nx.complete_graph(5)
 G2=nx.complete_graph(5)
 G2=nx.relabel_nodes(G2,{0:"A",1:"B",2:"C",3:"D",4:"E",5:"F"})
-G_connector=nx.from_edgelist([(4,"X"),("A","X")])
+G_connector=nx.from_edgelist([(4,"X"),("A","X"),
+                              (4,"F"),("F","G"),
+                              ("G","H"),("H","A")])
 G=nx.compose_all([G1,G2,G_connector])
 
 print(list(nx.bridges(G)))
+#? Đây là sao? Không hiện bridges
+print(list(nx.local_bridges(G)))
 
 nx.draw_spring(G,with_labels=True)
 plt.show()
