3f4fa47af08ffc7d8e81343d9f4d27583b7852d3
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-16 16:58:00 +0700

    Add Eulerian Path

---
+++ NetworkX Crash/Eulerian Path(10).py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+G1=nx.complete_graph(5)
+G2=nx.complete_graph(5)
+G2=nx.relabel_nodes(G2,{0:"A",1:"B",2:"C",3:"D",4:"E",5:"F"})
+G_connector=nx.from_edgelist([(4,"X"),("A","X")])
+G=nx.compose_all([G1,G2,G_connector])
+
+nx.draw_spring(G,with_labels=True)
+print(list(nx.eulerian_path(G)))
+plt.show()
