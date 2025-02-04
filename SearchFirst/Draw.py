0b372818c03e1e3a30e4c24df29d7f591b6a194f
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-02-04 15:39:45 +0700

    Fix BFS

--- SearchFirst/Draw.py
+++ SearchFirst/Draw.py
@@ -4,30 +4,51 @@ import time
 
 def visualize_search(order,edges ,title,G,pos):
     try:
-        plt.figure()
+        plt.figure(figsize=(8,6))
         plt.title(title)
+        
+        visited_edges=set()
+        
         for i, node in enumerate(order,start=1):
             plt.clf()
-            plt.title(title)
+            plt.title(f"{title} - Step{i}/{len(order)}") 
             
-            print(order)
-            
+            if i<=len(edges):
+                visited_edges.add(edges[i-1])           
             # Tô màu cạnh được duyệt
             edge_colors=[]
+            # list_edges_visited=list()
             for edge in G.edges():
                 if edge in edges[:i] or (edge[1],edge[0]) in edges[:i]:
                     edge_colors.append('yellow')
                 else:
-                    edge_colors.append('black')
-                print(edges)
+                    edge_colors.append('black')     
+                # list_edges_visited.append(edge)
+            
+            # Tô node
+            # Tô màu các node ban đầu bằng dict
+            node_colors={node:'green' for node in G.nodes()}
+            
+            # Tô màu các node sẽ duyệt
+            for edge in edges[:i]: # duyệt các cạnh đến cạnh i-1
+                # Tô node gốc
+                node_colors[edge[0]]='red'
+                # Tô node đỉnh
+                node_colors[edge[1]]='yellow'
+            # chuyển từ dict sang list
+            node_colors_list=[node_colors[node] for node in G.nodes()]
+            
             # Nếu muốn tô các 2 nút khác nhau bằng 2 màu khác nhau có thể thử enumerate
             nx.draw(G,pos,with_labels=True,
-                    node_color=['r' if n == node else 'g' for n in G.nodes()],
+                    node_color=node_colors_list,
                     edge_color=edge_colors,
+                    node_size=800,
+                    font_size=14,
+                    width=2,
                     )
             # plt.savefig('search_result/search_result_{}.png'.format(i))
             plt.draw()
-            plt.pause(3)
+            plt.pause(1.5)
         plt.show()
         time.sleep(3)
     except ValueError:
--- SearchFirst/Test.py
+++ SearchFirst/Test.py
@@ -20,13 +20,4 @@ if __name__ =='__main__':
     pos=nx.spring_layout(G)
     order,edges=Breadth_First_Search.order_bfs(G,start_node='1')
     
-    # nx.draw_networkx_nodes(G,pos,node_size=500,node_color='b')
-    # nx.draw_networkx_labels(G,pos,font_size=10,width=2,font_color='r')
-    
-    # all_edges=G.edges()
-    # edge_colors=['red' if edge in edges else 'black' for edge in all_edges]
-    # nx.draw_networkx_edges(G,pos,edge_color=edge_colors,arrows=True)
-    
     Draw.visualize_search(order=order, edges=edges,title='BFS Visualization',G=G,pos=pos)
-    # plt.legend()
-    # plt.show()
